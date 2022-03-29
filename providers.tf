95e4d64148d50a6405a6fbe8e71ec4a9ebc9533d
Your Name <you@example.com>
2022-03-29 16:16:36 +0000

    rootc  access key

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
   access_key = "AKIA2LDTAOI7G5ZHOEHH"
-  secret_key = "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"
+  secret_key = "ejPgl8W5iW6wX4MO4N1SMRd+LsgDja42wUvHULQC"
 }
 
 data "aws_region" "current" {
